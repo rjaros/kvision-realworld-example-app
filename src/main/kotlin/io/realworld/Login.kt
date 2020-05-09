ddc5974b200a45f398d120aab85795633718170f
Robert Jaros <rjaros@finn.pl>
2020-05-10 01:54:21 +0200

    Simplified login form

--- src/main/kotlin/io/realworld/Login.kt
+++ src/main/kotlin/io/realworld/Login.kt
@@ -1,16 +1,16 @@
 package io.realworld
 
 import pl.treksoft.kvision.core.Container
+import pl.treksoft.kvision.form.form
 import pl.treksoft.kvision.form.text.TextInput
 import pl.treksoft.kvision.form.text.TextInputType
 import pl.treksoft.kvision.form.text.textInput
-import pl.treksoft.kvision.html.TAG
 import pl.treksoft.kvision.html.button
 import pl.treksoft.kvision.html.div
+import pl.treksoft.kvision.html.fieldset
 import pl.treksoft.kvision.html.h1
 import pl.treksoft.kvision.html.link
 import pl.treksoft.kvision.html.p
-import pl.treksoft.kvision.html.tag
 import pl.treksoft.kvision.html.ul
 
 fun Container.loginPage(state: ConduitState) {
@@ -25,15 +25,15 @@ fun Container.loginPage(state: ConduitState) {
                     if (state.loginError) {
                         ul(listOf("email or password is invalid"), className = "error-messages")
                     }
-                    tag(TAG.FORM) {
+                    form {
                         lateinit var emailInput: TextInput
                         lateinit var passwordInput: TextInput
-                        tag(TAG.FIELDSET, className = "form-group") {
+                        fieldset(className = "form-group") {
                             emailInput = textInput(className = "form-control form-control-lg") {
                                 placeholder = "Email"
                             }
                         }
-                        tag(TAG.FIELDSET, className = "form-group") {
+                        fieldset(className = "form-group") {
                             passwordInput =
                                 textInput(TextInputType.PASSWORD, className = "form-control form-control-lg") {
                                     placeholder = "Password"
